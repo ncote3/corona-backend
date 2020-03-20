00d2dc3b5ad76a7c907c7de4cda0dcfe86ff4508
noahcote <noah.cote3@gmail.com>
2020-03-20 17:11:57 -0400

    First

---
+++ scraper/dataScraper/dataScraper.js
@@ -0,0 +1,34 @@
+exports = module.exports = dataScraper;
+function dataScraper () {
+    const axios = require('axios');
+    const cheerio = require('cheerio');
+
+    const scraperUrl = 'https://ncov2019.live/data';
+    let data = {};
+    return axios.get(scraperUrl)
+        .then(res => {
+            const $ = cheerio.load(res.data);
+            $('#sortable_table_Global tbody tr').each((i, elem) => {
+                const name = $(elem).find('.text--gray').html();
+                let popInfected = $(elem).find('.text--green').html();
+                let popCured = $(elem).find('.text--blue').html();
+                let popDead = $(elem).find('.text--red').html();
+
+                if (popInfected) {
+                    popInfected = parseInt(popInfected.replace(/,/g, ''), 10);
+                }
+                if (popCured) {
+                    popCured = parseInt(popCured.replace(/,/g, ''), 10);
+                }
+                if (popDead) {
+                    popDead = parseInt(popDead.replace(/,/g, ''), 10);
+                }
+                data[name.replace(' ', '')] = {name, popInfected, popCured, popDead}
+            });
+
+            return data;
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const dataScraper = require('./scraper/dataScraper/dataScraper');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const asyncMiddleware = fn =>
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch(next);
+    };
+
+app.get('/api/PersonGrid', asyncMiddleware(async (req, res) => {
+    const data = await dataScraper();
+
+    const response = {
+        country: {
+            usa: data['UnitedStates'],
+            france: data['France'],
+        },
+    };
+    res.send(response);
+}));
+
+app.post('/api/world', (req, res) => {
+    console.log(req.body);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
