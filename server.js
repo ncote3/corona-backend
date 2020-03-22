d30120a1f69ed867153a1d935255c1832b6d5e54
noahcote <noah.cote3@gmail.com>
2020-03-22 17:58:29 -0400

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const dataScraper = require('./scraper/dataScraper/dataScraper');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
